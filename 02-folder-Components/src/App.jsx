90d80e658976648dbd5596b7c1dd71ed998b65c0
Manzar Abbass <manzarja32@gmail.com>
2026-03-17 22:13:06 +0500

    Components Practice

---
+++ 02-folder-Components/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card"
+
+const App = () => {
+  return (
+    <>
+    <Card/>
+    <Card/>
+    <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ 02-folder-Components/src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>Manzar Abbass</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem tempora natus deleniti doloremque laboriosam architecto, cupiditate omnis vitae placeat eius.</p>
+        </div>
+    )
+}
+
+export default Card
