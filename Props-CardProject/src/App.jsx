182ca267279b4de22254555d5b3eb9fb0ba21bbb
Manzar Abbass <manzarja32@gmail.com>
2026-03-18 05:06:34 +0500

    Making Components of Cards and declare Data in App

---
+++ Props-CardProject/src/App.jsx
@@ -0,0 +1,15 @@
+import Cards from "./Components/Cards"
+
+const App = () => {
+
+  return (
+    <>
+    <div className="parent">
+      <Cards/>
+    </div>
+    </>
+
+  )
+}
+
+export default App
---
+++ Props-CardProject/src/Components/Cards.jsx
@@ -0,0 +1,14 @@
+const Cards = (arry) => {
+  return (
+    <div className="card">
+        <div className="top">
+            <img src="" alt="" />
+            <button>Save</button>
+        </div>
+        <div className="center"></div>
+    </div>
+    )
+
+  }
+
+export default Cards
