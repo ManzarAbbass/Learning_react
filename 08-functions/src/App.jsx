4ac3c0d23dabede81b5dc6ac7407cc3683a3132a
Manzar Abbass <manzarja32@gmail.com>
2026-03-24 13:44:30 +0500

    u create fnc then call in event so your have another option create don call fnc instead write fnc in it

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,15 +1,14 @@
 const App = () => {
-  let num=0;
   function BtnClick(){
-    console.log(num)
-    num++
-    console.log(num)
-
+    console.log("Button is Clicked")
+  }
+  function Entermouse(){
+    console.log("Mouse is Enter")
   }
   return (
-    <div>
-      <h1>Number is: {num}</h1>
-      <button onClick={BtnClick} className="bg-black py-5 px-8 text-white mt-1">Increase Number</button>
+    <div className="flex flex-col items-start">
+      <button onMouseEnter={Entermouse} onClick={BtnClick} className="bg-black py-5 px-8 text-white mt-1">Click Me</button>
+      {/* <button onMouseEnter={Entermouse} className="bg-orange-400 py-5 px-8 text-white mt-1">hover Me</button> */}
     </div>
   )
 }
