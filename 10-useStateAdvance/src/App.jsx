753b2594d9eaba0f8ea477213263b70f5b7b2ee2
Manzar Abbass <manzarja32@gmail.com>
2026-04-04 01:25:17 +0500

    coments on code for learning

--- 10-useStateAdvance/src/App.jsx
+++ 10-useStateAdvance/src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from "react"
 
 const App = () => {
   const [num, setNum] = useState(10);
-  const btnClicked=()=>{
+  const btnClicked = () => {
+    // setNum call karne ke baad state immediately update nahi hoti.
+    // React state ko schedule karta hai aur next render me update karta hai.
+    // Isliye same function ke andar num ki value purani (previous) hi rehti hai.
+
+    console.log(num) // phele bh print krdo tw 10 hi aygaw
     setNum(30);
     console.log(num); // yaha pe 30 print hona chaye tha apko lgta he lkn nhi yaha pe num ki initial value print hogi kiuke setNum jo he wo asynchronus way me kam krta he 
   }
