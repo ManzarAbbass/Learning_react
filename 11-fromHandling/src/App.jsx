18f8bca5b445b510c90812c6112f0350f425049d
Manzar Abbass <manzarja32@gmail.com>
2026-04-04 13:46:45 +0500

    learning about forms and work on it

---
+++ 11-fromHandling/src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  const submitHandler=(evtobj)=>{
+    evtobj.preventDefault();
+    console.log(evtobj.target[0].value)
+  }
+  return (
+    <div className="bg-black h-screen">
+      <form onSubmit={(e)=> {
+        submitHandler(e)
+      }}>
+        <input className="py-10 px-20 border border-white text-white text-[15px] mx-4" type="text" placeholder="Enter Your Name" />
+        <button className="py-5 px-10 bg-gray-700 border border-white rounded-2xl">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
