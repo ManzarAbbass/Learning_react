0fed2cfed4f0247b7e990b1820f76694f7f294e6
Manzar Abbass <manzarja32@gmail.com>
2026-04-04 13:09:48 +0500

    learn how to use spread Operator and prev arow fnc to work on obj,array

--- 10-useStateAdvance/src/App.jsx
+++ 10-useStateAdvance/src/App.jsx
@@ -22,10 +22,10 @@ import { useState } from "react"
 // export default App
 
 // ......working with Objects & Arrays in useState
-const App = () => {
-  const [user, setUser] = useState({name:"Manzar",age:22});
-  const [arr,setArr]=useState([10,20,30])
-  const changeUser=()=>{
+// const App = () => {
+//   const [user, setUser] = useState({name:"Manzar",age:22});
+//   const [arr,setArr]=useState([10,20,30])
+//   const changeUser=()=>{
     // two ways to update the state of an object in react
     // 1. setUser({name:"Shoaib",age:22}) isme humne pura object replace kr diya
     // const [a,...b]=arr
@@ -36,17 +36,53 @@ const App = () => {
     // newUser.name="Shoaib"
     // setUser(newUser)
 
-    const newArr=[...arr];
-    newArr.push(40);
-    setArr(newArr)
-  }
-  return (
-    <div className="bg-black text-white h-screen">
-      {/* <h1 className="font-bold text-5xl">{user.name}, {user.age}</h1> */}
-      <h1 className="font-bold text-5xl">{arr}</h1>
-      <button onClick={changeUser} className="bg-gray-500 rounded px-5 py-3">Change user</button>
-    </div>
-  )
-}
+    // const newArr=[...arr];
+    // newArr.push(40);
+    // setArr(newArr)
+//   }
+//   return (
+//     <div className="bg-black text-white h-screen">
+//       {/* <h1 className="font-bold text-5xl">{user.name}, {user.age}</h1> */}
+//       <h1 className="font-bold text-5xl">{arr}</h1>
+//       <button onClick={changeUser} className="bg-gray-500 rounded px-5 py-3">Change user</button>
+//     </div>
+//   )
+// }
 
+// export default App
+
+// const App=()=>{
+//   const [Array, setArray] = useState([10,20,30]);
+//   const [Obj, setObj] = useState({name:"Manzar",age:22});
+//   const changeObject=()=>{
+//     // const newObj={...Obj};
+//     // newObj.age=23;
+//     // setObj(newObj);
+
+//     // short working way 
+//     // setObj(prev=>({...prev,age:23}))
+//     setObj(prev=>({...prev,age:prev.age+1}))
+//   }
+//   const arrayChange=()=>{
+//     // const newArr=[...Array];
+//     // newArr.push(40);
+//     // setArray(newArr);
+
+//     // short working way
+//     // setArray(prev=>[...prev,50])
+//     // setArray(prev => [...prev, prev.length * 10]);
+//     setArray(prev => [...prev, Math.floor(Math.random() * 100)]);
+//   }
+//   return (
+//     <div>
+//       <h1 className="font-bold text-7xl">{Array}</h1>
+//       <button onClick={arrayChange} className="bg-red-500 px-4 py-2 rounded">change</button>
+//       <h2 className="font-bold text-7xl">{Obj.name},{Obj.age}</h2>
+//       <button onClick={changeObject} className="bg-blue-500 px-4 py-2 rounded">change</button>
+//     </div>
+//   )
+// }
+
+
+// Batch Update in React 
 export default App
