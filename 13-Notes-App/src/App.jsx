c662de0e192eccc24a19f1da3f5b66edff0fa705
Manzar Abbass <manzarja32@gmail.com>
2026-04-07 20:28:16 +0500

    create form & design it

--- 13-Notes-App/src/App.jsx
+++ 13-Notes-App/src/App.jsx
@@ -2,7 +2,21 @@ const App = () => {
   return (
     <div className="bg-black h-screen text-white">
       <div>
-        <h2>Notes App</h2>
+      <form className="flex flex-col gap-4 p-10">
+
+        <input 
+        type="text"
+        placeholder="Enter Notes Heading"
+        className="px-5 py-2 border-2 rounded"
+        />
+        <textarea 
+        placeholder="Enter Details"
+        name=""
+        id=""
+        className="px-5 py-2 border-2 rounded"
+         ></textarea>
+         <button className="bg-white text-black px-5 py-2 rounded">Add Note</button>
+      </form>
       </div>
       <div></div>
     </div>
