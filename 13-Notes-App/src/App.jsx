139ca089eeebe5cdf7b9bbab3050086d4b9ed494
Manzar Abbass <manzarja32@gmail.com>
2026-04-08 19:42:02 +0500

    make Notes card Container

--- 13-Notes-App/src/App.jsx
+++ 13-Notes-App/src/App.jsx
@@ -5,17 +5,27 @@ const App = () => {
   const submitHandler=(evtObj)=>{
     evtObj.preventDefault();
   }
+  const getHeading=(evtObj)=>{
+    setHeading(evtObj.target.value);
+  }
+  const getDetails=(evtObj)=>{
+    console.log(evtObj.target.value);
+  }
   return (
     <div className="bg-black h-screen text-white">
       <div>
       <form onSubmit={submitHandler} className="flex justify-between items-start p-10">
         <div className="flex flex-col w-1/2 items-start gap-4">
+        <h2>Notes Form</h2>
         <input 
+        onChange={getHeading}
         type="text"
         placeholder="Enter Notes Heading"
         className="px-5 py-2 border-2 rounded outline-none font-medium w-full"
+        value={cardheading}
         />
-        <textarea 
+        <textarea
+        onChange={getDetails} 
         placeholder="Enter Details"
         name=""
         id=""
@@ -25,7 +35,13 @@ const App = () => {
          </div>
       </form>
       </div>
-      <div></div>
+      <div>
+        <h2>Notes Cards</h2>
+        <div className="bg-white w-60">
+        <h3>Hello</h3>
+        <p>This is a simple note.</p>
+        </div>
+      </div>
     </div>
   )
 }
