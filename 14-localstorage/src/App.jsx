7ad7eece797fa9edaa524fe49e5f7c38823ed1b3
Manzar Abbass <manzarja32@gmail.com>
2026-04-11 19:11:32 +0500

    set object in localstorage using stringyfy method

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,3 +1,5 @@
+import { use } from "react"
+
 const App = () => {
 
   //... Set Item in localStorage and sessionStorage
@@ -22,8 +24,19 @@ const App = () => {
   // ...Clear() in localStorage and sessionStorage
   // {clear() ko hmm localStorage.clear() and sessionstorage.clear() aese likhna hota he ye pora localstorage aur sessionStorage k data ura deta he }
 
-  sessionStorage.clear();
-  localStorage.clear();
+  // sessionStorage.clear();
+  // localStorage.clear();
+
+  // //.... localStorage and sessionStorage with array and Object 
+  // {localStorage and sessionStorage me array aur object ko store krne ke liye hume unko string me convert krna hota he using JSON.stringify() and get krne ke liye JSON.parse() ka use krte hein}
+
+  const user={
+    username:"manzar",
+    age:23,
+    city:"Karachi"
+  }
+  // console.log(user)
+  localStorage.setItem("user",JSON.stringify(user))
 
   return (
     <div className="bg-black h-screen text-white">
