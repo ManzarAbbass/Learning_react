c0e9e7f3cab6b9b689aac770e2258560610cf191
Manzar Abbass <manzarja32@gmail.com>
2026-04-11 19:50:01 +0500

    we just test with functions how Api is calling next we use fetchAPi method

--- 15-API-calling/src/App.jsx
+++ 15-API-calling/src/App.jsx
@@ -1,7 +1,12 @@
 const App = () => {
+  const getData=()=>{
+    console.log("data is fetched...!")
+  }
   return (
-    <div>
-      
+    <div className="bg-black h-screen p-5">
+      <button onClick={()=>{
+        getData();
+      }} className="bg-red-500 text-white px-5 py-3 rounded">Get data</button>
     </div>
   )
 }
