869ef26b538c54451c3793052ac425c2aafb38d9
Manzar Abbass <manzarja32@gmail.com>
2026-04-11 15:59:52 +0500

    set comments for better understanding

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,14 +1,17 @@
 const App = () => {
 
   //... Set Item in localStorage and sessionStorage
+  // {setItem() me Key aur uski value dete hein kiuke set krnaw hota he ("name","manzar") aur value string me deni hoti he}
+
   // localStorage.setItem("programer","manzar");
   // sessionStorage.setItem("role","frontend developer");
 
   // ... Get Item from localStorage and sessionStorage
-
+    // {getItem() me Key dete hein kiuke get krnaw hota he data ("name")}
   const programer= localStorage.getItem("programer");
   console.log(programer);
   const role=sessionStorage.getItem("role");
+  console.log(role)
   return (
     <div className="bg-black h-screen text-white">
       App
