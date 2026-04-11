981be9efcfd0984b2cc1d16dad46ba9df8684451
Manzar Abbass <manzarja32@gmail.com>
2026-04-11 15:53:49 +0500

    get data from localstorage and sessionstorage

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,6 +1,14 @@
 const App = () => {
-  localStorage.setItem("programer","manzar");
-  sessionStorage.setItem("role","frontend developer");
+
+  //... Set Item in localStorage and sessionStorage
+  // localStorage.setItem("programer","manzar");
+  // sessionStorage.setItem("role","frontend developer");
+
+  // ... Get Item from localStorage and sessionStorage
+
+  const programer= localStorage.getItem("programer");
+  console.log(programer);
+  const role=sessionStorage.getItem("role");
   return (
     <div className="bg-black h-screen text-white">
       App
