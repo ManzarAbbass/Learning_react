cc9021b015a3bdabb26bd522e625e22375006712
Manzar Abbass <manzarja32@gmail.com>
2026-04-12 23:04:34 +0500

    comment on code for better understanding

--- 16-useEffect/src/App.jsx
+++ 16-useEffect/src/App.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react"
+// ...useEffect kiya hai? Ye ek hook hai jo functional components me side effects ko handle karne ke liye use hota hai. Side effects ka matlab hai ki aise operations jo component ke render hone ke baad perform hote hain, jaise ki data fetching, subscriptions, timers, etc.
 const App = () => {
+  // ....Wrong Way to use useEffect
+  // function random(){
+  //   const num=Math.random();
+  //   console.log(num);
+  // }
+  // random();
+
+
+  // const [num, setNum] = useState(0)
+  // function changeNum(){
+  //   setNum(num+1);
+  // }
+  // useEffect(function(){
+  //   console.log("Use Effect Called");
+  // })
+
+  // ... ye ap normal function se bh krskte ho tw ye useeffect ka proper use nhi hai useeffect ka kaam kuch aur he 
+
+  // {.....................usecase of useEffect
+  // 1. jab jab apki state change hui tab tab apki useEffect call hogi mtlb side effect horha hai manlo num state change ho rha hai to useEffect call hoga isko hmm bolte hein mounting 
+  //   const [num, setNum] = useState(0)
+  // function changeNum(){
+  //   setNum(num+1);
+  // }
+  // useEffect(function(){
+  //   console.log("Use Effect Called");
+  // })
+
+  // 2..apne agar dependency ka array pass krdia tw useEffect ek hi baar call hogaw 
+  const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(100)
+  function changeNum() {
+    setNum(num + 1);
+  }
+  useEffect(function () {
+    console.log("Use Effect Called");
+  }, [])
+  // }
+
+
+
   return (
-    <div>
-      
+    <div className="bg-black h-screen text-white">
+      <h1>value of num is :{num}</h1>
+      <h2>value of num2 is: {num2}</h2>
+      <button onClick={()=>{
+        changeNum();
+        setNum2(num2+10);
+      }} className="bg-red-500 p-4">Change Number</button>
     </div>
   )
 }
