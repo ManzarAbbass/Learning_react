a9f783d3aba6dc5555fb5123e1db0c21c80d1f0f
Manzar Abbass <manzarja32@gmail.com>
2026-04-13 00:13:02 +0500

    next example of useeffect

--- 16-useEffect/src/App.jsx
+++ 16-useEffect/src/App.jsx
@@ -30,27 +30,38 @@ const App = () => {
   // })
 
   // 2..apne agar dependency ka array pass krdia tw useEffect ek hi baar call hogaw 
-  const [num, setNum] = useState(0)
-  const [num2, setNum2] = useState(100)
-  function changeNum() {
-    setNum(num + 1);
-  }
+  // const [num, setNum] = useState(0)
+  // const [num2, setNum2] = useState(100)
+  // function changeNum() {
+  //   setNum(num + 1);
+  // }
   // useEffect(function () {
   //   console.log("Use Effect Called");
   // }, [])
 
   // {ab me charha hun jab jab num ki value change hogi tab tab useEffect call hoga mtlb num state ke change hone par side effect hoga to useEffect call hoga}
-  useEffect(function(){
-    console.log("Use Effect Called of num");
-  },[num])
-  //  hmm ne dependencies array me num pass krdiya mtlb jab num change ho wab useEffect chale 
+  // useEffect(function(){
+  //   console.log("Use Effect Called of num");
+  // },[num])
+  //  hmm ne  array me num dependencies pass krdiya mtlb jab num change ho wab useEffect chale 
   // }
 
 
-
+  //  ........................................................ Next Example
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+  function aChanging() {
+    console.log("A ki value change ho gayi");
+  }
+  function bChanging() {
+    console.log("B ki value change ho gayi");
+  }
+  useEffect(()=>{
+    console.log("Use Effect of A called");
+  },[a])
   return (
     <div className="bg-black h-screen text-white">
-      <h1>value of num is :{num}</h1>
+      {/* <h1>value of num is :{num}</h1>
       <h2>value of num2 is: {num2}</h2>
 
       <button onMouseEnter={()=>{
@@ -59,8 +70,17 @@ const App = () => {
       onMouseLeave={()=>{
         setNum2(num2+10);
       }} 
-      className="bg-red-500 p-4">Change Number</button>
+      className="bg-red-500 p-4">Change Number</button> */}
 
+      <h1>change value of a is :{a}</h1>
+      <button onClick={() => {
+        setA(a + 1)
+      }}
+        className="px-20 py-20 m-5 bg-gray-400">Change A</button>
+      <h1>change value of b is :{b}</h1>
+      <button onClick={() => {
+        setB(b - 1)
+      }} className="px-20 py-20 m-5 bg-gray-400">Change B</button>
     </div>
   )
 }
