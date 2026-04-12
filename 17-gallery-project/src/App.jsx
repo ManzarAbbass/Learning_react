b14fab1619b1efcf3af71aa3aa7870b338773e44
Manzar Abbass <manzarja32@gmail.com>
2026-04-13 01:51:36 +0500

    install axios for fetch data from api

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,7 +1,17 @@
 const App = () => {
+  const fetchData=async()=>{
+    // console.log("data a gya.....")
+    const response=
+  }
   return (
-    <div className="bg-black h-screen text-white">
-      App
+    <div className="bg-black h-screen p-4 text-white">
+      <button
+      onClick={()=>{
+        fetchData();
+      }}
+        className="bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded">
+        get data
+      </button>
     </div>
   )
 }
