becd94ca24b9454054b3ed060c09c289ad42de8d
Manzar Abbass <manzarja32@gmail.com>
2026-04-12 17:08:58 +0500

    data coming through axios save into usestate array

--- 15-API-calling/src/App.jsx
+++ 15-API-calling/src/App.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useState } from "react";
 const App = () => {
   // ...get data with fetch Api 
   // const getData=async ()=>{
@@ -9,16 +10,20 @@ const App = () => {
   // }
 
   // ..get data with axios
+  const [users, setUsers] = useState([]);
   const getData=async ()=>{
     const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-    const data=response.data;
-    console.log(data);
+    const data=await response.data;
+    setUsers(data);
   }
   return (
     <div className="bg-black h-screen p-5">
       <button onClick={()=>{
         getData();
       }} className="bg-red-500 text-white px-5 py-3 rounded">Get data</button>
+      {users.map(function(ueser,idx){
+        return "Hello"
+      })}
     </div>
   )
 }
