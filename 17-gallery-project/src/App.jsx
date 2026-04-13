45fa7071f7464ae515c3486443bfd5a961fbf8db
Manzar Abbass <manzarja32@gmail.com>
2026-04-13 16:07:54 +0500

    design the card for better look

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -2,30 +2,36 @@ import axios from "axios";
 import { useState } from "react";
 const App = () => {
   const [userdata, setUserData] = useState([])
-  const fetchData=async()=>{
+  const fetchData = async () => {
     // console.log("data a gya.....")
-    const response=await axios.get('https://picsum.photos/v2/list?page=1&limit=15')
-    const data=response.data;
+    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
+    const data = response.data;
     setUserData(data);
+    console.log(data);
   }
-  let isuserHave="No User is Available";
-  if(userdata.length>0){
-    isuserHave=userdata.map(function(user){
-      return <h1 key={user.id}>Hello,{user.id}</h1>
+  let isuserHave = "No User is Available";
+  if (userdata.length > 0) {
+    isuserHave = userdata.map(function (user) {
+      return <div>
+        <div className="h-40 w-44 overflow-hidden rounded-xl" key={user.id}>
+          <img className="h-full w-full object-cover" src={user.download_url} alt="img" />
+        </div>
+        <h2>{user.author}</h2>
+      </div>
     })
   }
 
   return (
     <div className="bg-black h-screen p-4 text-white">
       <button
-      onClick={()=>{
-        fetchData();
-      }}
+        onClick={() => {
+          fetchData();
+        }}
         className="bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded">
         Get data
       </button>
-      <div className="flex">
-        <div>{isuserHave}</div>
+      <div className="flex flex-wrap gap-4">
+        {isuserHave}
       </div>
     </div>
   )
