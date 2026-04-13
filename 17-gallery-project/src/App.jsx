5140175595da9bdeb3955cdf4765ef1c5598f25f
Manzar Abbass <manzarja32@gmail.com>
2026-04-14 00:26:30 +0500

    add buttons to reach out to next and prev page

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [userdata, setUserData] = useState([])
   const fetchData = async () => {
     // console.log("data a gya.....")
-    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
+    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=18')
     const data = response.data;
     setUserData(data);
     console.log(data);
@@ -17,7 +17,7 @@ const App = () => {
     isuserHave = userdata.map(function (user) {
       return <div key={user.id} className="flex flex-col items-center gap-2">
         <a href={user.url} target="_blank" rel="noreferrer">
-        <div className="h-40 w-44 overflow-hidden rounded-xl">
+        <div className="h-45 w-55 overflow-hidden rounded-xl">
           <img className="h-full w-full object-cover" src={user.download_url} alt="img" />
         </div>
         <h2 className="font-bold text-lg">{user.author}</h2>
@@ -26,9 +26,13 @@ const App = () => {
     })
   }
   return (
-    <div className="bg-black h-screen p-4 text-white">
-      <div className="flex flex-wrap gap-4">
+    <div className="bg-black  p-4 text-white">
+      <div className="flex flex-wrap gap-4 p-5 justify-center items-center">
         {isuserHave}
+        <div>
+        <button>Next</button>
+        <button>Prev</button>
+        </div>
       </div>
     </div>
   )
