7e3e420470f34513a7b050aaf316b17fcb31ec0d
Manzar Abbass <manzarja32@gmail.com>
2026-04-13 15:16:21 +0500

    show images on btn click logic

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,13 +1,20 @@
 import axios from "axios";
 import { useState } from "react";
 const App = () => {
-  const [data, setData] = useState([])
+  const [userdata, setUserData] = useState([])
   const fetchData=async()=>{
     // console.log("data a gya.....")
     const response=await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
     const data=response.data;
-    setData(data)
+    setData(data);
   }
+  let isHave="No Image is Available";
+  if(userdata.length>0){
+    isHave=userdata.map(function(user){
+      
+    })
+  }
+
   return (
     <div className="bg-black h-screen p-4 text-white">
       <button
@@ -17,6 +24,9 @@ const App = () => {
         className="bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded">
         Get data
       </button>
+      <div>
+        <div>{isHave}</div>
+      </div>
     </div>
   )
 }
