c0ea053644ca4c3294438bae825417e73ebaa691
Manzar Abbass <manzarja32@gmail.com>
2026-04-14 00:53:37 +0500

    more styling

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [userdata, setUserData] = useState([])
   const fetchData = async () => {
     // console.log("data a gya.....")
-    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=18')
+    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
     const data = response.data;
     setUserData(data);
     console.log(data);
@@ -15,7 +15,7 @@ const App = () => {
   let isuserHave = <h3 className="text-gray-400 text-xs">No User Available</h3>
   if (userdata.length > 0) {
     isuserHave = userdata.map(function (user) {
-      return <div key={user.id} className="flex flex-col items-center gap-2">
+      return <div key={user.id} className="flex flex-col items-center gap-2 p-2 bg-gray-800 rounded-xl">
         <a href={user.url} target="_blank" rel="noreferrer">
         <div className="h-45 w-55 overflow-hidden rounded-xl">
           <img className="h-full w-full object-cover" src={user.download_url} alt="img" />
@@ -27,12 +27,12 @@ const App = () => {
   }
   return (
     <div className="bg-black  p-4 text-white">
-      <div className="flex flex-wrap gap-4 p-5 justify-center items-center">
-        {isuserHave}
-        <div>
-        <button className="bg-green-500">Next</button>
-        <button className="bg-green-500">Prev</button>
+        <div className="flex gap-4 mt-5 justify-center">
+        <button className="bg-green-500 px-7 py-4 rounded">Next</button>
+        <button className="bg-green-500 px-7 py-4 rounded">Prev</button>
         </div>
+      <div className="flex flex-wrap gap-4 p-5 justify-center items-center h-screen overflow-auto">
+        {isuserHave}
       </div>
     </div>
   )
