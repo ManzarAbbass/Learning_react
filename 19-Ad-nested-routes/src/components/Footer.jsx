1f1766acf2ed495050277df386f24edefe117226
Manzar Abbass <manzarja32@gmail.com>
2026-04-16 21:02:02 +0500

    change footer content

--- 19-Ad-nested-routes/src/components/Footer.jsx
+++ 19-Ad-nested-routes/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <div className="absolute bottom-0 flex justify-center bg-fuchsia-500 w-full px-8 py-4">
-        <h2 className="font-bold">Footer</h2>
+        <h2 className="font-bold">This is Footer</h2>
     </div>
   )
 }
