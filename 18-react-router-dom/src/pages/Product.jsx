94d69616a57498e33d5d5f35fd3e141329078b46
Manzar Abbass <manzarja32@gmail.com>
2026-04-16 18:51:33 +0500

    make product component in pages

---
+++ 18-react-router-dom/src/pages/Product.jsx
@@ -0,0 +1,9 @@
+const Product = () => {
+  return (
+    <div>
+      <h1 className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 text-8xl underline">Product Page</h1>
+    </div>
+  )
+}
+
+export default Product
