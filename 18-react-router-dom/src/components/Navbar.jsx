b8e301e479d5652316454f2c231ee26e6b777c4d
Manzar Abbass <manzarja32@gmail.com>
2026-04-16 18:41:05 +0500

    so the page reload problem is solve

--- 18-react-router-dom/src/components/Navbar.jsx
+++ 18-react-router-dom/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div className='flex justify-between px-10 py-5 bg-fuchsia-500'>
       <h2>Navbar</h2>
       <div className='flex gap-8'>
-        <a href='/'>Home</a>
-        <a href='/about'>About</a>
-        <a href='/contact'>Contact</a>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </div>
       </div>
   )
