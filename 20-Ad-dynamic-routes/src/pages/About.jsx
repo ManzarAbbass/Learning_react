ac01f8525482f884b7feee48bb70ddaba3494928
Manzar Abbass <manzarja32@gmail.com>
2026-04-17 22:12:29 +0500

    best use of UseNavigate

--- 20-Ad-dynamic-routes/src/pages/About.jsx
+++ 20-Ad-dynamic-routes/src/pages/About.jsx
@@ -2,16 +2,19 @@ import { useNavigate } from "react-router-dom"
 
 const About = () => {
   const navg=useNavigate()
-  const btnClick=()=>{
-    navg('/')
-  }
   return (
     <div>
       <button 
       onClick={()=>{
-        btnClick()
+        navg('/')
       }} 
       className="bg-pink-800 px-5 py-2 rounded m-2 cursor-pointer">Return to home</button>
+      <button
+      onClick={()=>{
+        navg(-1)
+      }}
+      className="bg-pink-800 px-5 py-2 rounded m-2 cursor-pointer"
+      >Back</button>
       <h1 className="absolute top-[30%] left-[50%] transform -translate-x-1/2 translate-y-1/2 text-8xl underline">About Page</h1>
     </div>
   )
