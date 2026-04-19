0a075040561a7aa56e939d40ae495114dff0c935
Manzar Abbass <manzarja32@gmail.com>
2026-04-19 15:35:11 +0500

    successfully send the data to usercard page  with the help of props dealing

--- 21-UserManag-Dashboard/src/pages/Users.jsx
+++ 21-UserManag-Dashboard/src/pages/Users.jsx
@@ -11,14 +11,13 @@ const Users = () => {
     }
     fetchUsers()
   },[])
-  console.log(users)
   return (
     <div className="max-w-6xl mx-auto p-6">
       <div className="mb-5">
       <SearchBar/>
       </div>
     <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-3 gap-6">
-      <UserCard/>
+      <UserCard user={users}/>
     </div>
     </div>
   )
