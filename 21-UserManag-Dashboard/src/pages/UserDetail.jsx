8a99285d3cf9b07519ddb2c9d3bb3b559aaf563a
Manzar Abbass <manzarja32@gmail.com>
2026-04-20 22:28:48 +0500

    again polish UI

--- 21-UserManag-Dashboard/src/pages/UserDetail.jsx
+++ 21-UserManag-Dashboard/src/pages/UserDetail.jsx
@@ -20,11 +20,11 @@ const UserDetail = () => {
     fetchUser()
   },[id])
   
-  if(!user) return <h2 className="absolute top-[50%] left-[50%] font-bold text-3xl">{"Loading User....."}</h2>
+  if(!user) return <h2 className="absolute top-[50%] left-[40%] font-bold text-xl">{"Loading User....."}</h2>
 
   return (
-    <div className="h-screen flex justify-center items-center bg-amber-500"> 
-    <div className="bg-white flex flex-col items-center p-4 rounded-xl shadow hover:shadow-lg transition cursor-pointer ">
+    <div className="h-screen flex justify-center items-center bg-gray-100"> 
+    <div className="bg-white flex flex-col items-center gap-8 justify-between p-12 rounded-xl shadow hover:shadow-lg transition cursor-pointer h-auto w-auto">
       <div className="flex flex-col items-center gap-2">
         <h2 className="text-lg font-semibold">{user.name}</h2>
         <p className="text-sm text-gray-500">{user.email}</p>
