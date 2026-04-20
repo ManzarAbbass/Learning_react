f024d521bf4eb4ef7b65eba62f7acf1f29b58264
Manzar Abbass <manzarja32@gmail.com>
2026-04-20 23:06:22 +0500

    toggle favorite  icon fill logi

--- 21-UserManag-Dashboard/src/components/UserCard.jsx
+++ 21-UserManag-Dashboard/src/components/UserCard.jsx
@@ -2,13 +2,23 @@
 import { Link, useNavigate } from "react-router-dom"
 import UserDetail from "../pages/UserDetail"
 import { Star } from "lucide-react"
+import { useState } from "react"
 
 const UserCard = ({user}) => {
+  const [iconfill, setIconFill] = useState(false)
   const navigate=useNavigate()
+
+  const toggleFavorite=()=>{
+    setIconFill(!iconfill)
+  }
   return (
     <div className="bg-white flex flex-col items-center gap-6 p-5 rounded-xl shadow hover:shadow-lg transition cursor-pointer relative">
-        <button>
-          <Star className="absolute top-2 right-2" color="blue" fill="null" size={20}/>
+        <button 
+        onClick={()=>{
+          toggleFavorite()
+        }}
+        className="absolute top-2 right-2">
+          <Star color="blue" fill={iconfill?"blue":"none"} size={20}/>
         </button>
       <div className="flex flex-col items-center gap-2">
         <h2 className="text-lg font-semibold">{user.name}</h2>
