665ba660568cd02d2dffc84ec0c50fe897f47a6c
Manzar Abbass <manzarja32@gmail.com>
2026-04-21 21:06:43 +0500

    check logic

--- 21-UserManag-Dashboard/src/pages/Favorites.jsx
+++ 21-UserManag-Dashboard/src/pages/Favorites.jsx
@@ -1,32 +1,44 @@
 import { getUsers } from '../services/Services';
 import { useEffect, useState } from 'react';
 import { Star } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const Favorites = () => {
   const favItem = JSON.parse(localStorage.getItem("favItem"))
   const [favuser, setFavUser] = useState(null)
-  useEffect(()=>{
-    const fetchUsers = async ()=>{
+  useEffect(() => {
+    const fetchUsers = async () => {
       const data = await getUsers()
-      const Users= data.filter((u)=>(favItem.includes(u.id)))
+      const Users = data.filter((u) => (favItem.includes(u.id)))
       setFavUser(Users)
     }
     fetchUsers()
-  },[])
+  }, [])
 
-  if(!favuser) return <h2 className="absolute top-[50%] left-[40%] font-bold text-xl">{"Loading Fav User....."}</h2>
+  const navigate = useNavigate();
+  function goBackToUsers() {
+    navigate("/")
+  }
+
+  if (!favuser) return <h2 className="absolute top-[50%] left-[40%] font-bold text-xl">{"Loading Fav User....."}</h2>
   return (
-    favuser.map((u)=>{
-      <div className="bg-white flex flex-col items-center gap-6 p-5 rounded-xl shadow hover:shadow-lg transition cursor-pointer relative">
-        <Star/>
-        <div>
-          <h2>{u.name}</h2>
-          <p>{u.email}</p>
-          <p></p>
-        </div>
-        <button className=' bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700'>return home</button>
+  
+    <div className="bg-white flex flex-col items-center gap-6 p-5 rounded-xl shadow hover:shadow-lg transition cursor-pointer relative">
+      <button className='absolute top-2 right-2'>
+        <Star color="blue" fill="blue" size={20} />
+      </button>
+      <div>
+        <h2>{favuser.name}</h2>
+        <p>{favuser.email}</p>
+        <p>{favuser?.address?.city}</p>
       </div>
-    })
+      <button
+        onClick={() => {
+          goBackToUsers()
+        }}
+        className=' bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700'>return home</button>
+    </div>
+
   )
 }
 
