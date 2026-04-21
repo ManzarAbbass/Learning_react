fdc4e4fb6c23f697160daa732302e7b5a941717e
Manzar Abbass <manzarja32@gmail.com>
2026-04-22 01:08:45 +0500

    polish logic

--- 21-UserManag-Dashboard/src/pages/Favorites.jsx
+++ 21-UserManag-Dashboard/src/pages/Favorites.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Favorites = () => {
   const favItem = JSON.parse(localStorage.getItem("favItem"))
-  const [favuser, setFavUser] = useState(null)
+  const [favuser, setFavUser] = useState([])
   useEffect(() => {
     const fetchUsers = async () => {
       const data = await getUsers()
@@ -20,12 +20,12 @@ const Favorites = () => {
     navigate("/")
   }
 
-  if (!favuser) return <h2 className="absolute top-[50%] left-[40%] font-bold text-xl">{"Loading Fav User....."}</h2>
+  if (favItem === null) return <h2 className="absolute top-[50%] left-[40%] font-bold text-xl">{"Loading Fav User....."}</h2>
   if (favuser.length === 0) return <h2 className="absolute top-[50%] left-[40%] font-bold text-xl">{"No Favorite User Found....."}</h2>
   return (
     favuser.map((u)=>{
-      return <div className="max-w-6xl mx-auto p-6">
-        <h2 className='text-center font-bold text-3xl border-blue-600 border-solid'>Favourites User</h2>
+      <h2 className='text-center font-bold text-3xl border-blue-600 border-solid'>Favourites User</h2>
+      return 
           <div className='grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-6 m-6'>
           <div className="bg-white flex flex-col items-center gap-6 p-5 rounded-xl shadow hover:shadow-lg transition cursor-pointer relative">
       <button className='absolute top-2 right-2'>
@@ -41,7 +41,6 @@ const Favorites = () => {
           goBackToUsers()
         }}
         className=' bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700'>return home</button>
-    </div>
     </div>
     </div>
     })
