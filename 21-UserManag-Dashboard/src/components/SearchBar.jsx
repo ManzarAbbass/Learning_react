f5d0a522a825c8e786c88fda6f4a61241979566c
Manzar Abbass <manzarja32@gmail.com>
2026-04-22 23:19:05 +0500

    add event onchange in input field and make state value

--- 21-UserManag-Dashboard/src/components/SearchBar.jsx
+++ 21-UserManag-Dashboard/src/components/SearchBar.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const SearchBar = () => {
+  const [value, setValue] = useState("")
   return (
     <div>
-      <input className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" placeholder="Search users....." />     
+      <input 
+      onChange={(eveObj)=>{
+        setValue(eveObj.target.value)
+      }}
+      className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+      type="text"
+      value={value}
+      placeholder="Search users....."
+       />     
     </div>
   )
 }
