b7ca17df7832b6f1d125e3540b1196682c4b7b1a
Manzar Abbass <manzarja32@gmail.com>
2026-04-22 23:49:24 +0500

    working another way to implement logic

--- 21-UserManag-Dashboard/src/components/SearchBar.jsx
+++ 21-UserManag-Dashboard/src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
-const SearchBar = ({ getInput }) => {
+const SearchBar = ({ getInput, value }) => {
   return (
     <div>
       <input 
--- 21-UserManag-Dashboard/src/pages/Users.jsx
+++ 21-UserManag-Dashboard/src/pages/Users.jsx
@@ -30,10 +30,11 @@ const Users = () => {
     fetchUsers()
   }, [])
 
+  console.log(value)
   return (
     <div className="max-w-6xl mx-auto p-6">
       <div className="mb-5">
-        <SearchBar getInput={getInput} />
+        <SearchBar getInput={getInput} value={value} />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-6">
         {
