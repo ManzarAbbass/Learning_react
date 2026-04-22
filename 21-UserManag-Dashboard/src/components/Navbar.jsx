ce861ef7018c90840959056b178a092a563e7423
Manzar Abbass <manzarja32@gmail.com>
2026-04-22 17:13:23 +0500

    complete navbar logic

--- 21-UserManag-Dashboard/src/components/Navbar.jsx
+++ 21-UserManag-Dashboard/src/components/Navbar.jsx
@@ -36,18 +36,22 @@ const Navbar = () => {
       <div className={`fixed inset-0 bg-black/40 z-40 ${isOpen ? "block" : "hidden"}`}></div>
 
       {/* SIDEBAR */}
-      <div className="fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-50 transform -translate-x-full transition-transform duration-300">
+      <div className={`fixed top-0 left-[100] h-full w-64 bg-white shadow-lg z-50 transform -translate-x-full transition-transform duration-300 ${isOpen ? "translate-x-0" : ""}`}>
         
         {/* Close Button */}
         <div className="flex justify-end p-4">
-          <button>
-            <X size={28} />
+          <button 
+          onClick={()=>{
+            toggleSidebar()
+          }}
+          className="bg-blue-600 p-2 rounded-xl">
+            <X size={20} color="white"/>
           </button>
         </div>
 
         <div className="flex flex-col gap-6 px-6 mt-4 text-gray-700">
-          <Link to="/" className="hover:text-blue-600">Users</Link>
-          <Link to="/favorites" className="hover:text-blue-600">Favorites</Link>
+          <Link to="/" className="hover:text-blue-600 hover:bg-gray-100 p-2 rounded">Users</Link>
+          <Link to="/favorites" className="hover:text-blue-600 hover:bg-gray-100 p-2 rounded">Favorites</Link>
         </div>
       </div>
     </>
