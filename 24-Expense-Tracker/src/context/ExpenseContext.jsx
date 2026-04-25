09515b98b38900c15e0ce0f5c3c5fd93a820e826
Manzar Abbass <manzarja32@gmail.com>
2026-04-25 22:06:34 +0500

    wrap the App with Context

--- 24-Expense-Tracker/src/context/ExpenseContext.jsx
+++ 24-Expense-Tracker/src/context/ExpenseContext.jsx
@@ -1,23 +1,22 @@
 import { createContext, useEffect, useState } from "react"
 
-export const expenseContextData = createContext
+export const expenseContextData = createContext()
 const ExpenseContext = (Props) => {
-  const dataStore=localStorage.getItem("transaction" || [])
-  const [transaction, settransaction] = useState(JSON.parse(dataStore))
+  const dataStore=localStorage.getItem("transaction")
+  const [transaction, settransaction] = useState(dataStore?JSON.parse(dataStore): [])
   useEffect(()=>{
     localStorage.setItem("transactions",JSON.stringify(transaction))
   },[transaction])
 
   const addTransaction=(newTransaction)=>{
-    transaction.push(newTransaction)
-    localStorage.setItem("transaction",JSON.stringify(transaction))
+    settransaction([...transaction,newTransaction])
   }
 
   const deleteTransaction=(id)=>{
-    settransaction(transaction.filter((obj,index)=>(index !==id)))
+    settransaction(transaction.filter((item)=>(item.id !==id)))
     }
     return (
-      <expenseContextData.Provider value={ transaction,addTransaction,deleteTransaction}>
+      <expenseContextData.Provider value={{ transaction,addTransaction,deleteTransaction}}>
       {Props.children}
       </expenseContextData.Provider>
   )
--- 24-Expense-Tracker/src/main.jsx
+++ 24-Expense-Tracker/src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ExpenseContext from './context/ExpenseContext.jsx'
 
 createRoot(document.getElementById('root')).render(
+    <ExpenseContext>
     <App />
+    </ExpenseContext>
 )
