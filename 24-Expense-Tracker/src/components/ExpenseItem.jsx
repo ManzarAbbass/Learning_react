ca6033d24a5f8ed294c0706cc3c5bab475a02eb8
Manzar Abbass <manzarja32@gmail.com>
2026-04-28 23:27:28 +0500

    more polish logic

--- 24-Expense-Tracker/src/components/ExpenseItem.jsx
+++ 24-Expense-Tracker/src/components/ExpenseItem.jsx
@@ -2,12 +2,12 @@ import { Trash } from "lucide-react"
 const ExpenseItem = ({ item}) => {
   console.log(item)
   return (
-    <div  className="bg-red-400">
+    <div  className="bg-red-400 p-2">
     <ul className="flex gap-2">
-      <li>{item.id}</li>
-      <li>{item.type}</li>
-      <li>{item.category}</li>
-      <li>{item.amount}</li>
+      <li>{`id: ${item.id}`}</li>
+      <li>{`type: ${item.type}`}</li>
+      <li>{`category: ${item.category}`}</li>
+      <li>{`amount: ${item.amount}`}</li>
     </ul>
     <button><Trash size={20}/></button>
     </div>
--- 24-Expense-Tracker/src/components/ExpenseList.jsx
+++ 24-Expense-Tracker/src/components/ExpenseList.jsx
@@ -3,26 +3,26 @@ import { expenseContextData } from "../context/ExpenseContext"
 import ExpenseItem from "./ExpenseItem"
 
 const ExpenseList = () => {
-    const { transaction } =useContext(expenseContextData)
+    const { transaction } = useContext(expenseContextData)
 
-    if(transaction.length === 0){
+    if (transaction.length === 0) {
         return (
             <div className="text-center mt-8">
                 <p className="text-2xl font-bold">No transactions yet</p>
             </div>
         )
     }
-  return (
-    <div className="bg-black">
-        <h2 className="text-2xl font-bold text-white mb-4">Transaction List</h2>
-        {transaction.map((item,index)=>{
-            <div key={index}>
-                <ExpenseItem item={item} />
-            </div>
-        })
-        }
-    </div>
-  )
+    return (
+        <div className="bg-black">
+            <h2 className="text-2xl font-bold text-white mb-4">Transaction List</h2>
+            {transaction.map((item, index) => {
+                return <div key={index}>
+                    <ExpenseItem item={item} />
+                </div>
+            })
+            }
+        </div>
+    )
 }
 
 export default ExpenseList
