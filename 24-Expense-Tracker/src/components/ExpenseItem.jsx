610c0895af8231971d537949403496ff8458c0ad
Manzar Abbass <manzarja32@gmail.com>
2026-04-30 02:19:41 +0500

    complete the delete logic of category wise

--- 24-Expense-Tracker/src/components/ExpenseItem.jsx
+++ 24-Expense-Tracker/src/components/ExpenseItem.jsx
@@ -1,6 +1,6 @@
 import { Trash } from "lucide-react"
 const ExpenseItem = ({ item}) => {
-
+  const { deleteTransaction } = useContext(expenseContextData)
   return (
     <div  className={`text-white flex gap-4 p-2 ${item.type === 'income' ? 'bg-green-500' : 'bg-red-500'}`} >
     <ul className="flex gap-2">
@@ -9,7 +9,11 @@ const ExpenseItem = ({ item}) => {
       <li>{`category: ${item.category}`}</li>
       <li>{`amount: ${item.amount}`}</li>
     </ul>
-    <button><Trash size={20}/></button>
+    <button
+    onClick={()=>{
+      deleteTransaction(item.id)
+    }}
+     ><Trash size={20}/></button>
     </div>
   )
 }
