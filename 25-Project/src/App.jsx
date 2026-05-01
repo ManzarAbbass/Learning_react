77dd296f1735740339aee279c703d4b02e84d84b
Manzar Abbass <manzarja32@gmail.com>
2026-05-01 23:08:24 +0500

    make login form in Auth folder

--- 25-Project/src/App.jsx
+++ 25-Project/src/App.jsx
@@ -1,9 +1,10 @@
+import Login from "./components/Auth/Login"
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Login />
+    </>
   )
 }
 
--- 25-Project/src/components/Auth/Login.jsx
+++ 25-Project/src/components/Auth/Login.jsx
@@ -1,7 +1,15 @@
 const Login = () => {
   return (
-    <div>
-      
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="border-2 border-red-600">
+        <form className="flex flex-col items-center justify-center">
+          <input  className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-white" type="email" placeholder="Enter your email" />
+          <input 
+          className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-white mt-3"
+          type="password" placeholder="Enter password" />
+          <button className="text-white outline-none border-none bg-emerald-600 text-xl py-3 px-5 rounded-full pla">Log in</button>
+        </form>
+      </div>
     </div>
   )
 }
