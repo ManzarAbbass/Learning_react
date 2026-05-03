c9df126dadbd4592ce8a666521d1bc4d293c55db
Manzar Abbass <manzarja32@gmail.com>
2026-05-03 23:49:08 +0500

    Ui is complete of login page

--- 25-Project/src/components/Auth/Login.jsx
+++ 25-Project/src/components/Auth/Login.jsx
@@ -7,15 +7,14 @@ const Login = () => {
 
         }} 
         className="flex flex-col items-center justify-center">
-          <input  className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-gray-400" type="email" placeholder="Enter your email" />
-          <input 
-          className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-gray-400 mt-3"
+          <input required className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-gray-400" type="email" placeholder="Enter your email" />
+          <input required className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-gray-400 mt-3"
           type="password" placeholder="Enter password" />
           <button 
           onClick={()=>{
 
           }}
-          className="text-white outline-none border-none bg-emerald-600 text-xl py-3 px-5 rounded-full mt-5">Log in</button>
+          className="text-white outline-none border-none bg-emerald-600 text-xl py-3 px-5 rounded-full mt-5 w-full">Log in</button>
         </form>
       </div>
     </div>
