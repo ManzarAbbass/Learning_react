8023ee31738e27ff69c7e2786c50ba4f1f930ced
Manzar Abbass <manzarja32@gmail.com>
2026-05-03 23:44:24 +0500

    login component UI polish

--- 25-Project/src/components/Auth/Login.jsx
+++ 25-Project/src/components/Auth/Login.jsx
@@ -1,13 +1,21 @@
 const Login = () => {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <div className="border-2 border-red-600">
-        <form className="flex flex-col items-center justify-center">
-          <input  className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-white" type="email" placeholder="Enter your email" />
+    <div className="h-screen w-screen flex items-center justify-center text-white">
+      <div className="border-2 border-red-600 p-20 rounded-2xl">
+        <form
+        onSubmit={()=>{
+
+        }} 
+        className="flex flex-col items-center justify-center">
+          <input  className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-gray-400" type="email" placeholder="Enter your email" />
           <input 
-          className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-white mt-3"
+          className="border-2 border-emerald-600 bg-transparent py-4 px-5 text-xl outline-none rounded-full placeholder:text-gray-400 mt-3"
           type="password" placeholder="Enter password" />
-          <button className="text-white outline-none border-none bg-emerald-600 text-xl py-3 px-5 rounded-full pla">Log in</button>
+          <button 
+          onClick={()=>{
+
+          }}
+          className="text-white outline-none border-none bg-emerald-600 text-xl py-3 px-5 rounded-full mt-5">Log in</button>
         </form>
       </div>
     </div>
