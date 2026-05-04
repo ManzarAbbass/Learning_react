163afa0fc9d03001856e33afc0acd0e27ada402c
Manzar Abbass <manzarja32@gmail.com>
2026-05-04 22:35:18 +0500

    design admin dashboard Ui

--- 25-Project/src/components/Dashboard/AdminDashboard.jsx
+++ 25-Project/src/components/Dashboard/AdminDashboard.jsx
@@ -1,7 +1,24 @@
+import Header from "../others/Header"
+
 const AdminDashboard = () => {
   return (
-    <div>
-      
+    <div className="h-screen w-full p-10 text-white">
+      <Header/>
+      <div>
+        <form >
+          <h3>Task Title</h3>
+          <input type="text" placeholder="Make a UI design" />
+          <h3> Description</h3>
+          <textarea name="" id="" cols="30" rows="5" placeholder="Enter task description"></textarea>
+          <h3>Date</h3>
+          <input type="date" />
+          <h3>Asign to</h3>
+          <input type="text" placeholder="employee name"/>
+          <h3>Category</h3>
+          <input type="text" placeholder="design, dev, etc"/>
+          <button>Create Task</button>
+        </form>
+      </div>
     </div>
   )
 }
