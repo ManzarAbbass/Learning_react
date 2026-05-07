0a4cb20dfcde1a1611a1c7ba8e72b17b1f22529b
Manzar Abbass <manzarja32@gmail.com>
2026-05-07 22:36:06 +0500

    UI task is compltd and now doing logic

--- 25-Project/src/components/TaskList/CompleteTask.jsx
+++ 25-Project/src/components/TaskList/CompleteTask.jsx
@@ -1,8 +1,16 @@
 const CompleteTask = () => {
   return (
-    <div>
-      
-    </div>
+      <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
+        <div className='flex justify-between items-center'>
+        <h3 className='bg-green-600 px-3 py-1 text-sm rounded-md'> High</h3>
+        <h4 className='text-sm'>20 feb 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube vedio</h2>
+        <p className='text-sm mt-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, debitis officiis vel magni fugit beatae.</p>
+        <div className="mt-2">
+          <button className="w-full">Completed</button>
+        </div>
+      </div>
   )
 }
 
--- 25-Project/src/components/TaskList/NewTask.jsx
+++ 25-Project/src/components/TaskList/NewTask.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const NewTask = () => {
   return (
-    <div>
-      
-    </div>
+      <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
+        <div className='flex justify-between items-center'>
+        <h3 className='bg-blue-600 px-3 py-1 text-sm rounded-md'> High</h3>
+        <h4 className='text-sm'>20 feb 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube vedio</h2>
+        <p className='text-sm mt-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, debitis officiis vel magni fugit beatae.</p>
+        <div className='mt-4'>
+            <button>
+                <button>Accept Task</button>
+            </button>
+        </div>
+      </div>
   )
 }
 
