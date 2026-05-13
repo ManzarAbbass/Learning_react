0b9d80c22aeae6a087798a322c7f8d5bb9684d96
Manzar Abbass <manzarja32@gmail.com>
2026-05-13 22:55:29 +0500

    counterSlice complete and export to it to the Store

--- Redux/src/redux/features/counterSlice.js
+++ Redux/src/redux/features/counterSlice.js
@@ -1,8 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const counterSlice=createSlice({
+export const counterSlice=createSlice({ // counterSlice hmne feature banaya count ka 
     name:"counter",
-    initialState:{
+    initialState:{ // state he hamri jiski value hmne initial 0 di he
         value:0
+    },
+    reducers:{ // iske reducer ke andar actions hein do increament or decreament 
+        increament:((state)=>state.value+=1),
+        decreament:((state)=>state.value-=1)
     }
 })
+
+export const {increament,decreament}=counterSlice.actions // ye hmne destructering ki he counterSlice.actions (incr,decr) aur usko export kiya he 
+export default counterSlice.reducer // aur ye hmne pore counterSlice ke andar jo object tha reducer ka usko export kiya default
