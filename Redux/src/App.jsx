f07fccf5f5841e0c31286f8a1feb423252ae46c1
Manzar Abbass <manzarja32@gmail.com>
2026-05-13 23:08:33 +0500

    designing

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,9 +1,9 @@
 const App = () => {
   return (
     <div className="text-black">
-      <h1>0</h1>
-      <button>increament</button>
-      <button>decreament</button>
+      <h1 className="text-3xl font-bold p-4 bg-gray-400 w-fit">0</h1>
+      <button className="px-2 py-1 m-4 bg-amber-400">increament</button>
+      <button className="px-2 py-1 m-4 bg-blue-400">decreament</button>
     </div>
   )
 }
