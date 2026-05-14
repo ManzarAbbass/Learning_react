7edea8136380197cc2ea75c35cd431e26c652031
Manzar Abbass <manzarja32@gmail.com>
2026-05-14 16:58:23 +0500

    subjectSlice done

--- 26-Project/src/features/subjects/subjectsSlice.js
+++ 26-Project/src/features/subjects/subjectsSlice.js
@@ -2,9 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const subjectsSlice = createSlice({
     name:"subjects",
-    initialState:{  //apki state
-        value:[]
-    },
+    initialState:[],
+    reducers:{
+        addSubject:(state,actions)=>{state.push(actions.payload)}
+    }
 })
 
+export const {addSubject} =subjectsSlice.actions
 export default subjectsSlice.reducer
--- 26-Project/src/store/store.js
+++ 26-Project/src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { subjectsSlice } from "../features/subjects/subjectsSlice"
+import { subjectsSliceReducer } from "../features/subjects/subjectsSlice"
+
 export const store= configureStore({
     reducer:{
-        subjects:subjectsSlice
+        subjects:subjectsSliceReducer
     }
 })
