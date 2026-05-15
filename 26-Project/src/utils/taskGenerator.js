982886cd0abd124d7326cb60b515f88cae900046
Manzar Abbass <manzarja32@gmail.com>
2026-05-15 22:14:04 +0500

    Polish logic TaskGenerator.js

--- 26-Project/src/utils/taskGenerator.js
+++ 26-Project/src/utils/taskGenerator.js
@@ -1,5 +1,15 @@
 const generateTask=(subject)=>{
-    if(subject.priority.toLowerCase()==="hard" && subject.difficulty.toLowerCase()==="hard"){
-        
+    // phele check krna he ke subject title is equla task subject
+    if(subject.priority.toLowerCase()==="high" && subject.difficulty.toLowerCase()==="hard"){
+        // hard task show krne hein
+    }
+    else if(subject.priority.toLowerCase()==="medium" && subject.difficulty.toLowerCase()==="medium"){
+        // medium task show krne hein
+    }
+    else if(subject.priority.toLowerCase()==="easy" && subject.difficulty.toLowerCase()==="low"){
+        // low task show krne hein
+    }
+    else{
+        //fallback generic task show krne hein 
     }
 }
