687a5ca3678cc72cf183bee01332f4d3345b34bf
Manzar Abbass <manzarja32@gmail.com>
2026-05-17 23:03:37 +0500

    priority logic is done

--- 26-Project/src/utils/taskGenerator.js
+++ 26-Project/src/utils/taskGenerator.js
@@ -12,14 +12,24 @@ const generateTask = (subject) => {
         }]
     }
 
+    let taskCount=1
     let difficulty =subject.difficulty.toLowerCase()
     const selectedTasks= templates[difficulty]
-    return selectedTasks.map((task,idx)=>({
-            id:Date.now() + idx,
-            title:task,
-            completed:false,
-            subjectId:subject.id
-    }))
+    let priority = subject.priority.toLowerCase()
+    if(priority==="high"){
+        taskCount=4; // reassign krdiya
+    }
+    else if(priority==="medium"){
+        taskCount=2
+    }
+    const priorityTask=selectedTasks.slice(0,taskCount)
+    return priorityTask.map((task,idx)=>({
+    id:Date.now() + idx,
+    title:task,
+    completed:false,
+    subjectId:subject.id
+}))
+
 }
 
 export default generateTask
