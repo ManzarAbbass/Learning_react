d7163e73dafab2542c4f3f101e6534969fd504ee
Manzar Abbass <manzarja32@gmail.com>
2026-05-17 18:08:02 +0500

    now working on priority based task logic

--- 26-Project/src/utils/taskGenerator.js
+++ 26-Project/src/utils/taskGenerator.js
@@ -1,15 +1,25 @@
-const generateTask=(subject)=>{
-    // phele check krna he ke subject title is equla task subject
-    if(subject.priority.toLowerCase()==="high" && subject.difficulty.toLowerCase()==="hard"){
-        // hard task show krne hein
+import taskData from "../features/tasks/taskData"
+
+const data = taskData
+const generateTask = (subject) => {
+    const templates = data[subject.subjectname]
+    if (!templates) {
+        return [{
+            id: Date.now(),
+            title: `Study ${subject.subjectname}`,
+            completed: false,
+            subjectId: subject.id
+        }]
     }
-    else if(subject.priority.toLowerCase()==="medium" && subject.difficulty.toLowerCase()==="medium"){
-        // medium task show krne hein
-    }
-    else if(subject.priority.toLowerCase()==="low" && subject.difficulty.toLowerCase()==="easy"){
-        // low task show krne hein
-    }
-    else{
-        //fallback generic task show krne hein 
-    }
+
+    let difficulty =subject.difficulty.toLowerCase()
+    const selectedTasks= templates[difficulty]
+    return selectedTasks.map((task,idx)=>({
+            id:Date.now() + idx,
+            title:task,
+            completed:false,
+            subjectId:subject.id
+    }))
 }
+
+export default generateTask
